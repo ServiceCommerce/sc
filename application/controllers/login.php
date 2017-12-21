ab46d909fd92abd6fd227e6319d4119f5395d065
Claudio Magalhães <claudio.magalhaes@outlook.com.br>
2017-12-21 10:58:44 -0200

    criação de login e rota

---
+++ application/controllers/login.php
@@ -0,0 +1,19 @@
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dinho
+ * Date: 21/12/2017
+ * Time: 10:52
+ */
+
+class login extends CI_Controller {
+
+    public function __construct(){
+        parent::__construct();
+    }
+
+    public function index(){
+        $this->load->view('client/login');
+    }
+}
