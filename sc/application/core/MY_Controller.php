706ca6d7256cd9794e6d1b0e855ebe675d65fac0
Claudio Magalhães <claudio.magalhaes@outlook.com.br>
2018-01-06 15:55:52 -0200

    adicção se super classe MY_controller

---
+++ sc/application/core/MY_Controller.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dinho
+ * Date: 06/01/2018
+ * Time: 14:05
+ */
+
+class MY_Controller extends CI_Controller {
+
+    function __construct(){
+        parent::__construct();
+    }#End construct
+
+}
