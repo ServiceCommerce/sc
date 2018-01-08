9200f50a67f7d0045235c2c00db5f15d17249132
Claudio Magalhães <claudio.magalhaes@outlook.com.br>
2018-01-08 16:29:01 -0200

    criação da classe Web.php

---
+++ application/controllers/Web.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dinho
+ * Date: 08/01/2018
+ * Time: 16:21
+ */
+
+class Web extends CI_Controller {
+
+    function __construct(){
+        parent::__construct();
+    }#End __construct()
+
+}#End class
