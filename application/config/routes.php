43d0c44f85b1f83632089b36a884b64d1855c89e
Claudio Magalhães <claudio.magalhaes@outlook.com.br>
2018-01-08 18:17:17 -0200

    rota home para web/home

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,3 +52,5 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'sc/sc_painel';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+$route['home'] = 'web/home';
