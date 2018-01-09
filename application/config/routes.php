7b6187c0ab43c5a722382035feb8a4ef622a9a5b
Claudio Magalhães <claudio.magalhaes@outlook.com.br>
2018-01-08 23:26:19 -0200

    ajuste de rota

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,7 +49,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'sc/sc_painel';
+$route['default_controller'] = (ENVIRONMENT !== 'production')? 'sc/sc_painel' : 'web/home';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 
